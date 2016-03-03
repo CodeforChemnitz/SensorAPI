ae6c2276f3a96015a35a4f4fc4440d8b4ae8212c
PhiBo <phibo@dinotools.org>
2016-03-03 10:28:36 +0100

    Move migration management to run() function

--- sensor_api/__init__.py
+++ sensor_api/__init__.py
@@ -10,13 +10,17 @@ app = Flask(__name__, instance_relative_config=True)
 app.config.from_object("sensor_api.default_settings")
 app.config.from_envvar("SENSOR_API_SETTINGS", silent=True)
 db = SQLAlchemy(app)
-migrate = Migrate(app, db)
 manager = Manager(app)
-manager.add_command("db", MigrateCommand)
 
 def run():
     #db.init_db()
     #app.run(host='0.0.0.0', threaded=True)
+    # Setup migration
+    Migrate(app, db)
+    manager.add_command("db", MigrateCommand)
+
+    # Load blueprints
     from .api import api_bp
     app.register_blueprint(api_bp)
+
     manager.run()
