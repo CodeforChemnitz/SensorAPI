661c2aa428d3aba6fb4f52f79f562117aa5802bd
PhiBo <phibo@dinotools.org>
2016-03-08 22:37:27 +0100

    Move api v1 to /api/v1

--- sensor_api/api/__init__.py
+++ sensor_api/api/__init__.py
@@ -7,7 +7,7 @@ from . import controllers
 api_bp = Blueprint("api", __name__)
 api = Api(api_bp)
 
-api.add_resource(controllers.UsersResource, "/users")
-api.add_resource(controllers.UserConfimResource, "/users/<int:id>/<approval_code>")
-api.add_resource(controllers.SensorNodes, "/nodes")
-api.add_resource(controllers.SensorNodeMetrics, "/nodes/<node_id>/metrics")
+api.add_resource(controllers.UsersResource, "/v1/users")
+api.add_resource(controllers.UserConfimResource, "/v1/users/<int:id>/<approval_code>")
+api.add_resource(controllers.SensorNodes, "/v1/nodes")
+api.add_resource(controllers.SensorNodeMetrics, "/v1/nodes/<node_id>/metrics")
