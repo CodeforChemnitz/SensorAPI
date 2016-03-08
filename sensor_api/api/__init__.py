83d6f42391c49520b91d724de8b1df15fd568285
PhiBo <phibo@dinotools.org>
2016-03-08 22:43:00 +0100

    Handle Rest API root node

--- sensor_api/api/__init__.py
+++ sensor_api/api/__init__.py
@@ -1,12 +1,28 @@
 # -*- coding: utf-8 -*-
 from flask import Blueprint
-from flask_restful import Api
+from flask_restful import Api, Resource
 
 from . import controllers
 
 api_bp = Blueprint("api", __name__)
 api = Api(api_bp)
 
+
+class ApiResource(Resource):
+    """
+    Handle the root node of the Rest API
+    """
+    def get(self):
+        """
+        Return a list of available API versions
+
+        :return: List of available API versions
+        :rtype: str[]
+        """
+        return ["v1"]
+
+
+api.add_resource(ApiResource, "/")
 api.add_resource(controllers.UsersResource, "/v1/users")
 api.add_resource(controllers.UserConfimResource, "/v1/users/<int:id>/<approval_code>")
 api.add_resource(controllers.SensorNodes, "/v1/nodes")
