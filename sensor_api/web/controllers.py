47b0fead57b7f4689d024b35fee07fd0acc0857f
PhiBo <phibo@dinotools.org>
2016-03-08 23:21:34 +0100

    Remove deprecated function to get sensor nodes

--- sensor_api/web/controllers.py
+++ sensor_api/web/controllers.py
@@ -1,8 +1,4 @@
-import json
-
 import flask
-
-from .. import models
 
 web_bp = flask.Blueprint("web", __name__)
 
@@ -12,18 +8,3 @@ def index():
     return flask.render_template(
         "web/index.html"
     )
-
-
-@web_bp.route("/locations", methods=["GET"])
-def locations():
-    nodes = {}
-    for node in models.SensorNode.query.all():
-        if not node.geo_latitude or not node.geo_longitude:
-            continue
-
-        # ToDo: encode Decimal()
-        nodes[node.id] = {
-            "geo_lat": float(node.geo_latitude),
-            "geo_lng": float(node.geo_longitude)
-        }
-    return json.dumps(nodes)
